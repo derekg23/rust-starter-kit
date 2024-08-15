bb216831c09d346a95ae38f028654370b6c0fe71
derek <dgalbraith523@gmail.com>
2024-08-15 14:00:35 -0400

    update to use database_url

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -14,14 +14,8 @@ impl DbConn {
 }
 
 pub fn establish_connection() -> DbConn {
-  let db_username = std::env::var("DB_USERNAME").expect("DB_USERNAME must be set");
-  let db_password = std::env::var("DB_PASSWORD").expect("DB_PASSWORD must be set");
-  let db_host = std::env::var("DB_HOST").expect("DB_HOST must be set");
-  let db_port = std::env::var("DB_PORT").expect("DB_PORT must be set");
-  let db_name = std::env::var("DB_NAME").expect("DB_NAME must be set");
-  let manager = ConnectionManager::<PgConnection>::new(
-    format!("postgres://{}:{}@{}:{}/{}", db_username, db_password, db_host, db_port, db_name)
-  );
+  let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  let manager = ConnectionManager::<PgConnection>::new(db_url);
   let pool = Pool::builder().build(manager).expect("Failed to create pool.");
   DbConn(pool)
 }
